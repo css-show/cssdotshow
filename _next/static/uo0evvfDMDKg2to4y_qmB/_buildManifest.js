878de21d3f6c810ac65ed60e3b7841067765ed9d
zhanghecool <zhanghecool@outlook.com>
2021-04-07 08:21:18 +0000

    Deploying to gh-pages from  @ 8c399b1e2108a17169647a687951ff0f8e16601e 🚀

---
+++ _next/static/uo0evvfDMDKg2to4y_qmB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/36c6fd2e449da5bcd1cb.css","static/chunks/pages/index-c123f047b2f3c487d5a8.js"],"/_error":["static/chunks/pages/_error-ef6758052658c0a9ee0e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
