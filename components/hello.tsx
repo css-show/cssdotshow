cf20264e840d8fb5cef29aebdcbc09d1fd25bd9a
张赫 <he.zhang@vcg.com>
2021-04-26 20:08:36 +0800

    feat(apollo): 增加apollo graphql

--- components/hello.tsx
+++
@@ -1,3 +0,0 @@
-export default function Hello() {
-  return <p>你好，秀儿！</p>;
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,134 @@
+import { FC } from 'react';
 import Head from 'next/head';
-import Hello from '@/components/hello';
+import Link from 'next/link';
+import Date from '@/components/date';
+import Layout, { siteTitle } from '@/components/layout';
 import styles from '@/styles/Home.module.scss';
+import utilStyles from '@/styles/utils.module.scss';
+import { getAllPostIds, getSortedPostsData } from '@/lib/posts';
+import { getApolloClient } from '@/lib/apollo';
+import { useQuery, gql } from '@apollo/client';
 
-export default function Home() {
+const QUERY = gql`
+  query GetLaunches {
+    launchesPast(limit: 100) {
+      id
+      mission_name
+      launch_date_local
+      launch_site {
+        site_name_long
+      }
+      links {
+        article_link
+        video_link
+        mission_patch
+      }
+      rocket {
+        rocket_name
+      }
+    }
+  }
+`;
+
+// interface LaunchesItem {
+//   id: string;
+//   mission_name: string;
+//   launch_date_local: string;
+//   launch_site: {
+//     site_name_long: string;
+//   };
+//   links: {
+//     article_link: string;
+//     video_link: string;
+//     mission_patch: string;
+//   };
+//   rocket: {
+//     rocket_name: string;
+//   };
+// }
+
+// export interface HomeProps {
+//   launches: LaunchesItem[];
+// }
+
+const Home = () => {
+  const { data, loading, error } = useQuery(QUERY);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  const launches = data.launchesPast || [];
+
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>CSS Show | 层叠样式表秀场</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main className={styles.main}>
-        <Hello />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {launches.map(({ id, mission_name, launch_date_local }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{mission_name}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={launch_date_local} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
+};
+
+export async function getStaticProps() {
+  // const allPostsData = getSortedPostsData();
+  const apolloClient = getApolloClient({});
+  // console.info('ApolloClient: ', apolloClient);
+  const { data, loading, error } = await apolloClient.query({
+    query: gql`
+      query GetLaunches {
+        launchesPast(limit: 10) {
+          id
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+            mission_patch
+          }
+          rocket {
+            rocket_name
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      launches: data.launchesPast,
+    },
+  };
 }
+
+export default Home;
