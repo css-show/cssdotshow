fc7914dfe7853c20c903569f3b3689b96f6576f6
张赫 <he.zhang@vcg.com>
2021-04-30 18:01:50 +0800

    fix(id.tsx): 修复文章详情页面多余逻辑

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,58 +3,7 @@ import { getAllPostIds, getPostData } from '@/lib/posts';
 import Layout from '@/components/layout';
 import { gql } from '@apollo/client';
 import Date from '@/components/date';
-import { getApolloClient } from '@/lib/apollo';
 import utilStyles from '@/styles/utils.module.scss';
-
-const QUERY = gql`
-  query GetLaunches($id: ID!) {
-    launch(id: $id) {
-      id
-      mission_id
-      mission_name
-      details
-      launch_success
-      launch_date_utc
-      is_tentative
-      launch_date_local
-      launch_date_unix
-      launch_site {
-        site_id
-        site_name
-        site_name_long
-      }
-      launch_year
-      links {
-        article_link
-        flickr_images
-        mission_patch
-        mission_patch_small
-        presskit
-        reddit_campaign
-        reddit_launch
-        reddit_media
-        reddit_recovery
-        video_link
-        wikipedia
-      }
-      static_fire_date_unix
-      static_fire_date_utc
-      telemetry {
-        flight_club
-      }
-      tentative_max_precision
-      upcoming
-    }
-  }
-`;
-
-const QUERYLIST = gql`
-  query GetLaunchesID {
-    launchesPast(limit: 10) {
-      id
-    }
-  }
-`;
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
