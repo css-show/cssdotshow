befad44d9c52e5303d256a02b167c800af5c8d7b
张赫 <he.zhang@vcg.com>
2021-07-20 11:01:31 +0800

    perf(rss.tsx): 优化 RSS 模版文件

--- lib/rss.tsx
+++ lib/rss.tsx
@@ -5,8 +5,9 @@ const EMAIL = 'zhanghecool@outlook.com';
 const PODCAST_NAME = 'The CSS Show';
 const SITE_URL = 'https://css.show';
 const DESCRIPTION = '只需要一分钟就能学会，却要用一辈子的时间去精通。';
-const LANGUAGE = 'zh-cn';
+const LANGUAGE = 'chi';
 const CATEGORY = 'Technology';
+const TYPE = 'serial'; // episodic，serial
 const PODCAST_IMAGE =
   'https://cdn.jsdelivr.net/gh/zhanghecool/cssdotshow@gh-pages/images/profile.jpg';
 const generateRssItem = ({
@@ -52,10 +53,16 @@ const generateRssItem = ({
 const generateRss = (
   posts: Post[]
 ): string => `<?xml version="1.0" encoding="UTF-8"?>
-<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:anchor="https://anchor.fm/xmlns">
   <channel>
     <title><![CDATA[${PODCAST_NAME}]]></title>
     <description><![CDATA[${DESCRIPTION}]]></description>
+    <link>${SITE_URL}</link>
+    <image>
+      <url>${PODCAST_IMAGE}</url>
+      <title>${PODCAST_NAME}</title>
+      <link>${SITE_URL}</link>
+    </image>
     <itunes:image href="${PODCAST_IMAGE}" />
     <language>${LANGUAGE}</language>
     <itunes:category text="${CATEGORY}" />
@@ -63,7 +70,10 @@ const generateRss = (
     <itunes:author>
       <![CDATA[${AUTHOR}]]>
     </itunes:author>
-    <link>${SITE_URL}</link>
+    <itunes:summary>
+      <![CDATA[${DESCRIPTION}]]>
+    </itunes:summary>
+    <itunes:type>${TYPE}</itunes:type>
     <itunes:owner>
       <itunes:name>
         <![CDATA[${AUTHOR}]]>
@@ -76,13 +86,10 @@ const generateRss = (
       <![CDATA[${AUTHOR} @${PODCAST_NAME}]]>
     </copyright>
     <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-    <atom:link href="${SITE_URL}/rss.xml" rel="self" type="application/rss+xml"/>
+    <atom:link href="${SITE_URL}/rss" rel="self" type="application/rss+xml"/>
     <itunes:subtitle>
       <![CDATA[${PODCAST_NAME}]]>
     </itunes:subtitle>
-    <itunes:summary>
-      <![CDATA[${DESCRIPTION}]]>
-    </itunes:summary>
     ${posts.map(generateRssItem).join('')}
   </channel>
 </rss>
