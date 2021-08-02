a6e1b4312a2a8f812dd4c1eac69e68f568ab00bb
张赫 <he.zhang@vcg.com>
2021-08-02 18:12:32 +0800

    docs(meta): upgrade meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,6 +31,7 @@ class AppDocument extends Document {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#264de4" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#FFFFFF" />
 
@@ -63,9 +64,57 @@ class AppDocument extends Document {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
         <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/images/profile.svg" color="#5bbad5" />
         <link rel="shortcut icon" href="/favicon.ico" />
+
         <Head />
         <body className="min-h-full">
           <Main />
