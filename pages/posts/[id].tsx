b2fa3c2ddeae1fd76f783335687d60d2f683bd56
张赫 <he.zhang@vcg.com>
2021-08-05 11:30:15 +0800

    fix(apollo): 删除 Apollo graphql 相关代码，只保留 PDF 生成文章详情页面

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import { getAllPostIds, getPostData } from '@/lib/posts';
 import Layout from '@/components/layout';
-import { gql } from '@apollo/client';
 import Date from '@/components/date';
 import utilStyles from '@/styles/utils.module.scss';
 
